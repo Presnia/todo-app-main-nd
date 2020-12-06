bb501cc7f3f9153ecb6e883c127ff3d615552180
Andrei Dzebysh <presnia@yahoo.com>
2020-12-06 20:58:58 +0300

    An attempt to create date format function

--- src/model/todo.js
+++ src/model/todo.js
@@ -1,10 +1,34 @@
 import todoState from "./todoState.js";
 
+let date = new Date();
+
+/* function formatDate() {
+  
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+  
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+  
+    let yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+
+    let hh = date.getHours();
+    if (hh < 10) hh = '0' + hh;
+
+    let ii = date.getMinutes();
+    if (ii < 10) ii = '0' + ii;
+  
+    return dd + '.' + mm + '.' + yy + ' - ' + hh + ":" + ii;
+  }
+
+
+console.log(formatDate()); */
 export default class Todo {
   constructor(text) {
     this.text = text;
     this.state = todoState.InProcess;
-    this.dateCreated = new Date();
+    this.dateCreated = date;
     this.dateCompleted = null;
   }
 
