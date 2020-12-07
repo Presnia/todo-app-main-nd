0ee7b5f6d77d288c4ae6be871db0403d44e96e89
Andrei Dzebysh <presnia@yahoo.com>
2020-12-07 21:40:59 +0300

    Adjusted date  format for panel (v.1)

--- src/view/todoListPage/todo.js
+++ src/view/todoListPage/todo.js
@@ -21,13 +21,23 @@ function renderStatusPanel(doc, todo) {
 }
 
 function formatDateForPanel(prefix, date) {
-  const datePart = `${date.getDate()}.${
-    date.getMonth() + 1
-  }.${date.getFullYear()}`;
 
-  const timePart = `${date.getHours()}:${date.getMinutes()}`;
+  let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+  
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+  
+    let yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
 
-  return `${prefix}: ${datePart} - ${timePart}`;
+    let hh = date.getHours();
+    if (hh < 10) hh = '0' + hh;
+
+    let ii = date.getMinutes();
+    if (ii < 10) ii = '0' + ii;
+  
+    return  `${prefix}: ${dd + '.' + mm + '.' + yy + ' - ' + hh + ":" + ii}`;
 }
 
 function renderCreatedDatePanel(doc, todo) {
