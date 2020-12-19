1f0ee6fa94301a2a7540041940c94aa979a70bf6
Andrei Dzebysh <presnia@yahoo.com>
2020-12-19 16:39:45 +0300

    Error-first callback pattern

--- src/demo.js
+++ src/demo.js
@@ -2,15 +2,22 @@ function loadScript(scriptSrc, loadedCallback) {
   let script = document.createElement("script");
   script.src = scriptSrc;
 
-  script.onload = () => loadedCallback(scriptSrc);
+  script.onload = () => loadedCallback(null, scriptSrc);
+  script.onerror = () => loadedCallback(new Error(`Script ${scriptSrc} loading failed`));
 
   document.head.append(script);
 }
 
+// error-first callback pattern
+
 export function demo() {
-  loadScript("/src/probe.js", (src) => {
+  loadScript("/src/probes/probe.js", (error, src) => {
+    if(error) {
+      console.log(error); 
+      return;
+    }
+
     console.log(`=> Script ${src} loaded...`);
-    
     probe();
   });
 }
