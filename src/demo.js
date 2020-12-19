efffa31285e8fbc101f9cebc66323bba87e20a3e
Andrei Dzebysh <presnia@yahoo.com>
2020-12-19 17:01:30 +0300

    Dynamically loading files (pattern)

--- src/demo.js
+++ src/demo.js
@@ -1,23 +1,24 @@
-function loadScript(scriptSrc, loadedCallback) {
+function loadScript(scriptSrc, loadedCallback, errorCallback) {
   let script = document.createElement("script");
   script.src = scriptSrc;
 
-  script.onload = () => loadedCallback(null, scriptSrc);
-  script.onerror = () => loadedCallback(new Error(`Script ${scriptSrc} loading failed`));
+  script.onload = () => loadedCallback(scriptSrc);
+  script.onerror = () => 
+  errorCallback(new Error(`Script ${scriptSrc} loading failed`));
 
   document.head.append(script);
 }
 
-// error-first callback pattern
-
 export function demo() {
-  loadScript("/src/probes/probe.js", (error, src) => {
-    if(error) {
-      console.log(error); 
-      return;
+  loadScript(
+    "/src/probes/probe1.js", 
+    (src) => {
+      console.log(`=> Script ${src} loaded...`);
+      probe(); 
+    },
+    (error) => {
+      console.log("### Load failed...");
+      console.log(error);
     }
-
-    console.log(`=> Script ${src} loaded...`);
-    probe();
-  });
+  );
 }
