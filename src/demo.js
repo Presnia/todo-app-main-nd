046765302b4d7e2b119c4884d8d02b3fe0b4f5ca
Andrei Dzebysh <presnia@yahoo.com>
2020-12-19 16:15:36 +0300

    loadScript have no callback function

--- src/demo.js
+++ src/demo.js
@@ -2,9 +2,14 @@ function loadScript(scriptSrc) {
   let script = document.createElement("script");
   script.src = scriptSrc;
 
+  script.onload = () => console.log(`### Script ${scriptSrc} loaded`);
+
   document.head.append(script);
 }
 
 export function demo() {
   loadScript("/src/probe.js");
+
+  console.log("=> Script loaded...");
+  probe();
 }
--- src/probe.js
+++ src/probe.js
@@ -1,1 +1,5 @@
 console.log("===> Hello, I am probe!!!");
+
+function probe() {
+  console.log("===> I am function inside probe.js");
+}
